6b2c273f1e230042a7b626fad9823e693c7e7779
Zzzul <807fahmi@gmail.com>
2022-10-03 21:26:59 +0700

    update unit test

--- tests/Feature/DistributionNewsTest.php
+++ tests/Feature/DistributionNewsTest.php
@@ -11,7 +11,7 @@ class DistributionNewsTest extends TestCase
     /**
      * @test
      */
-    public function get_latest_12_news_and_1_sponsor_news()
+    public function can_get_latest_news()
     {
         $this->get('/api/v2/news')
             ->assertStatus(200)
@@ -21,7 +21,7 @@ class DistributionNewsTest extends TestCase
     /**
      * @test
      */
-    public function get_news_by_id()
+    public function can_get_news_by_id()
     {
         $this->get('/api/v2/news/11531')
             ->assertStatus(200);
@@ -30,7 +30,7 @@ class DistributionNewsTest extends TestCase
     /**
      * @test
      */
-    public function get_all_news_params()
+    public function can_get_news_params()
     {
         $this->get('/api/v2/params/news/')
             ->assertStatus(200);
@@ -39,7 +39,7 @@ class DistributionNewsTest extends TestCase
     /**
      * @test
      */
-    public function search_news_with_avaiable_params()
+    public function can_search_news()
     {
         $this->get('/api/v2/news?distribution=ubuntu&release=stable&month=all&year=2021')
             ->assertStatus(200)
